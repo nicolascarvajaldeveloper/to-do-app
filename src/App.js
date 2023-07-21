57a38fa422a968df8ae547c456ae5508c1cdd775
nicolascarvajaldeveloper <nicolascarvajaldeveloper@gmail.com>
2023-07-21 17:27:58 -0500

    to do app

---
+++ src/App.js
@@ -0,0 +1,117 @@
+import { useEffect, useState } from 'react';
+
+import { Title, TodoInput, TodoList } from './components';
+
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Ver la última temporada de DARK 🎭 ',
+      completed: false,
+    },
+    {
+      id: 2,
+      title: 'Completar el portafolio de Full Stack👨🏼‍💻 ',
+     completed: false,
+    },
+    {
+      id: 3,
+      title: 'Ir al gimnasio🤸‍♂️ ',
+      completed: false,
+    },
+    {
+      id: 4,
+      title: 'Completar los trabajos de la universidad 🤓',
+      completed: false,
+    }
+  ])
+
+  const [activeFilter, setActiveFilter] = useState('all');
+
+  const [filteredTodos, setFilteredTodos] = useState(todos);
+
+  const addTodo = (title) => {
+    const lastId = todos.length > 0 ? todos[todos.length - 1].id : 1;
+
+    const newTodo = {
+      id: lastId + 1,
+      title,
+      completed: false
+    }
+
+    const todoList = [...todos]
+    todoList.push(newTodo);
+
+    setTodos(todoList);
+  }
+
+  const handleSetComplete = (id) => {
+
+    const updatedList = todos.map(todo => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed}
+      }
+      return todo;
+    })
+
+    setTodos(updatedList);
+  } 
+
+  const handleClearComplete = () => {
+    const updatedList = todos.filter(todo => !todo.completed);
+    setTodos(updatedList);
+  };
+
+  const handleDelete = (id) => {
+    const updatedList = todos.filter(todo => todo.id !== id);
+    setTodos(updatedList);
+  }
+
+  const showAllTodos = () => {
+    setActiveFilter('all')
+  }
+
+  const showActiveTodos = () => {
+    setActiveFilter('active')
+  }
+
+  const showCompletedTodos = () => {
+    setActiveFilter('completed')
+  }
+  
+  useEffect(() => {
+    if (activeFilter === 'all') {
+      setFilteredTodos(todos);
+    } else if (activeFilter === 'active') {
+        const activeTodos = todos.filter(todo => todo.completed === false);
+        setFilteredTodos(activeTodos);
+    } else if (activeFilter === 'completed') {
+        const completedTodos = todos.filter(todo => todo.completed === true);
+        setFilteredTodos(completedTodos);
+    }
+    
+  },[activeFilter, todos]);
+
+  return (
+    <div className='bg-gray-900 min-h-screen font-inter h-full text-gray-100 flex items-center justify-center py-20 px-5'>
+      <div className='container flex flex-col max-w-xl'>
+        <Title />
+        <TodoInput addTodo={addTodo} />
+        <TodoList
+          activeFilter={activeFilter}
+          todos={filteredTodos}
+          showAllTodos={showAllTodos}
+          showActiveTodos={showActiveTodos}
+          showCompletedTodos={showCompletedTodos}
+          handleSetComplete={handleSetComplete}
+          handleDelete={handleDelete}
+          handleClearComplete={handleClearComplete} />
+      </div>
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,9 @@
+const Title = () => {
+    return (
+        <h1 className="text-5xl font-anton font-bold tracking-widest">
+            App de Tareas Pendientes
+        </h1>
+    );
+}
+
+export { Title }
